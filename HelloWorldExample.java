b5388bf3fc7d9a5f0f6a94d40ceabe24051a5a56
Prasanth Kusumaraju <pkusumaraju@rythmos.com>
2018-07-06 15:37:43 +0530

    commit

--- HelloWorldExample.java
+++ HelloWorldExample.java
@@ -6,12 +6,17 @@ public class HelloWorldExample{
  
   public static void main(String args[]){
  
-    /*
-    Use System.out.println() to print on console.
-    */
+   
     System.out.println("Hello World !");
   
   }
+ 
+ 
+ void add(){
+	 
+	  System.out.println("Sum "+ (5+4));
+	 
+ }
  
 }
  
